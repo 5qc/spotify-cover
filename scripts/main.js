862ee13586b716a3cff95b8220af90c1d0973c79
ro <roc0ast3r@gmail.com>
2021-12-14 08:29:51 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ $("a").attr("target", "_blank");
 
 // Auto-generate placeholder photos
 if (document.getElementById("fileUpload").files.length == 0) {
-  $("#cover-img").attr("src", "//picsum.photos/1000/1000");
+  $("#cover-img").attr("src", "//picsum.photos/id/69/1000/1000");
   $("#cover").click(function() {
     var d = new Date();
     $("#cover-img")
@@ -37,43 +37,28 @@ $("#text").keyup(function() {
 $("#position").change(function() {
   if ($("#position").val() == "top-left") {
     $("#cover-text")
-      .removeClass()
-      .addClass("top-left");
-    $("#cover-logo")
-      .removeClass()
+      .removeClass("top-left top-right bottom-left bottom-right center")
       .addClass("top-left");
   }
   if ($("#position").val() == "top-right") {
     $("#cover-text")
-      .removeClass()
+      .removeClass("top-left top-right bottom-left bottom-right center")
       .addClass("top-right");
-    $("#cover-logo")
-      .removeClass()
-      .addClass("default");
   }
   if ($("#position").val() == "bottom-left") {
     $("#cover-text")
-      .removeClass()
+      .removeClass("top-left top-right bottom-left bottom-right center")
       .addClass("bottom-left");
-    $("#cover-logo")
-      .removeClass()
-      .addClass("default");
   }
   if ($("#position").val() == "bottom-right") {
     $("#cover-text")
-      .removeClass()
+      .removeClass("top-left top-right bottom-left bottom-right center")
       .addClass("bottom-right");
-    $("#cover-logo")
-      .removeClass()
-      .addClass("default");
   }
   if ($("#position").val() == "center") {
     $("#cover-text")
-      .removeClass()
+      .removeClass("top-left top-right bottom-left bottom-right")
       .addClass("center");
-    $("#cover-logo")
-      .removeClass()
-      .addClass("default");
   }
 });
 
@@ -100,24 +85,64 @@ $("#textColor").change(function() {
 $("#logoColor").change(function() {
   if ($("#logoColor").val() == "auto") {
     $("#cover-logo")
-      .removeClass("auto black white")
+      .removeClass("auto black white none transparent")
       .addClass("auto");
   }
   if ($("#logoColor").val() == "black") {
     $("#cover-logo")
-      .removeClass("auto black white")
+      .removeClass("auto black white none transparent")
       .addClass("black");
+  }
+  if ($("#logoColor").val() == "black-transparent") {
+    $("#cover-logo")
+      .removeClass("auto black white none transparent")
+      .addClass("black transparent")
   }
   if ($("#logoColor").val() == "white") {
     $("#cover-logo")
-      .removeClass("auto black white")
+      .removeClass("auto black white none transparent")
       .addClass("white");
+  }
+  if ($("#logoColor").val() == "white-transparent") {
+    $("#cover-logo")
+      .removeClass("auto black white none transparent")
+      .addClass("white transparent")
+  }
+  if ($("#logoColor").val() == "none") {
+    $("#cover-logo")
+      .removeClass("auto black white none transparent")
+      .addClass("none");
   }
 });
 
+// Convert blob image to data
+function blob2canvas(canvas, blob) {
+  var img = new Image();
+  var ctx = canvas.getContext("2d");
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+  }
+  img.src = blob;
+}
+
 // Change cover div to image
 $("#download").click(function() {
-  html2canvas(document.querySelector("#cover")).then(canvas => {
-    $("#previewImage").append(canvas);
+  html2canvas(document.querySelector("#cover"), {
+    useCORS: true,
+    onrendered: function(canvas) {
+      var context = canvas.getContext("2d");
+
+      if ($("#cover-img").attr("src") !== $('#coverimg[^="blob"]')) {
+        var img = new Image();
+        img.src = document.getElementById("cover-img").src;
+        img.onload = context.drawImage(img, 0, 0, 600, 600);
+      } else {
+        blob2canvas(canvas, $("#cover-img").attr("src"))
+      }
+    }
+  }).then(canvas => {
+    $("#previewImage")
+      .empty()
+      .append(canvas);
   });
 });
